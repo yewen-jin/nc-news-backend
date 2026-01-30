d8c0066b884bf23143a4b79b5f6e931b6504a011
Yewen Jin <elsaurum@gmail.com>
2026-01-30 13:11:45 +0000

    created dev data for emojis and ascii-animals

---
+++ db/data/development-data/ascii-animals.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    animal: "cat",
+    drawing: `
+ /\_/\
+( o.o )
+ > ^ <
+`,
+  },
+  {
+    animal: "fish",
+    drawing: `
+|\   \\\\__     o
+| \_/    o \    o
+> _   (( <_  oo
+| / \__+___/
+|/     |/
+`,
+  },
+];
--- db/data/development-data/index.js
+++ db/data/development-data/index.js
@@ -1,4 +1,6 @@
-exports.articleData = require('./articles.js');
-exports.commentData = require('./comments.js');
-exports.topicData = require('./topics.js');
-exports.userData = require('./users.js');
+exports.articleData = require("./articles.js");
+exports.commentData = require("./comments.js");
+exports.topicData = require("./topics.js");
+exports.userData = require("./users.js");
+exports.emojiData = require("./emojis.js");
+exports.asciiData = require("./ascii-animals.js");
