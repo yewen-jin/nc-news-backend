8011862ef4d7f7f2e1cd69bdedaca07402ff95d5
Yewen Jin <elsaurum@gmail.com>
2026-02-04 14:46:44 +0000

    change join to left join to get articles with 0 comments

--- db/get-tables.sql
+++ db/get-tables.sql
@@ -24,4 +24,4 @@
 -- SELECT * FROM user_topic;
 -- SELECT * FROM user_article_votes;
 
-SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id) AS comment_count FROM articles JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY articles.created_at DESC;
+SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY articles.created_at DESC;
