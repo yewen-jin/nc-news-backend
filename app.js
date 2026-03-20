810c024dadb1aad00fca6e1ad732b338ba38b045
yewen-jin <yewen.elsa.jin@gmail.com>
2026-03-20 11:21:27 +0000

    add root path to documentation

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const apiRouter = require("./routes/api-router");
 const NotFoundError = require("./errors/not-found-error");
 const InvalidInputError = require("./errors/invalid-input-error");
 const {
-  genericErrorHandler,
-  invalidPathsHandler,
+    genericErrorHandler,
+    invalidPathsHandler,
 } = require("./errors/error-handler");
 
 const app = express();
@@ -15,6 +15,7 @@ app.use(express.json());
 
 // VALID PATHS
 // The middleware chain only go into any of these if the endpoint matches the argument string
+app.use("/", express.static("public"));
 app.use("/docs", express.static("public"));
 app.use("/api", apiRouter);
 
@@ -25,14 +26,14 @@ app.all("/*path", invalidPathsHandler);
 // Error handling middleware functions
 // if there are any errors in the middle of the above functions, using next(err) will pass the err object into the next middleware function
 app.use((err, req, res, next) => {
-  if (err instanceof NotFoundError) {
-    // console.log("404 error: ", err);
-    res.status(404).send({ msg: err.message });
-  } else if (err instanceof InvalidInputError) {
-    res.status(400).send({ msg: err.message });
-  } else {
-    next(err);
-  }
+    if (err instanceof NotFoundError) {
+        // console.log("404 error: ", err);
+        res.status(404).send({ msg: err.message });
+    } else if (err instanceof InvalidInputError) {
+        res.status(400).send({ msg: err.message });
+    } else {
+        next(err);
+    }
 });
 
 //INTERNAL ERROR
