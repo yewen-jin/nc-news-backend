6e89192672333beea11388304cf8ab6d2b4238d1
Yewen Jin <yewen.elsa.jin@gmail.com>
2026-03-24 12:53:18 +0000

    added back deleted line

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/", express.static("public"));
 app.use("/docs", express.static("public"));
 app.use("/api", apiRouter);
 app.all("/*path", invalidPathsHandler);
