282d6d494e1c6f047ec18ae8c2d208b6a85161ba
Yewen Jin <yewen.elsa.jin@gmail.com>
2026-03-24 12:47:32 +0000

    remove unecessary comments and files

--- app.js
+++ app.js
@@ -13,21 +13,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// VALID PATHS
-// The middleware chain only go into any of these if the endpoint matches the argument string
-app.use("/", express.static("public"));
 app.use("/docs", express.static("public"));
 app.use("/api", apiRouter);
-
-// INVALID PATHS
-// Errors in the previous function wouldn't go into this catch app path function because next(err) contains one argument, and it will go to the next middleware function that has 4 arguments, whereas this one has 3
 app.all("/*path", invalidPathsHandler);
 
-// Error handling middleware functions
-// if there are any errors in the middle of the above functions, using next(err) will pass the err object into the next middleware function
 app.use((err, req, res, next) => {
     if (err instanceof NotFoundError) {
-        // console.log("404 error: ", err);
         res.status(404).send({ msg: err.message });
     } else if (err instanceof InvalidInputError) {
         res.status(400).send({ msg: err.message });
@@ -36,7 +27,6 @@ app.use((err, req, res, next) => {
     }
 });
 
-//INTERNAL ERROR
 app.use(genericErrorHandler);
 
 module.exports = app;
